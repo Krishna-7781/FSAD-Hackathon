a70d39497445af16aae41bc41c0ecf190f0b2011
A Sudarsan Krishna <askrish2803@gmail.com>
2026-04-08 14:44:50 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -19,7 +19,7 @@ function Register() {
   const [citizenId, setCitizenId] = useState("");
 
   useEffect(() => {
-    axios.get("http://localhost:5000/centers")
+    axios.get("https://fsad-hackathon-1.onrender.com/api/centers")
       .then(res => setCenters(res.data))
       .catch(() => alert("Error loading centers"));
   }, []);
